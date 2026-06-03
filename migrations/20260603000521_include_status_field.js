8d80691990ef3df9314f249284357b408c8d3f29
Cristian Henao <krishenaosanta@hotmail.com>
2026-06-02 19:11:53 -0500

    refactor: retrict tournament without dates

--- migrations/20260603000521_include_status_field.js
+++ migrations/20260603000521_include_status_field.js
@@ -2,14 +2,34 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function (knex) {
+  await knex.raw(`
+    CREATE TYPE tournament_status AS ENUM ('active', 'inactive', 'terminated')
+  `);
+
+  await knex.raw(`
+    ALTER TABLE tournaments
+    ADD COLUMN status tournament_status NOT NULL DEFAULT 'active'
+  `);
+
+  await knex.raw(`
+    ALTER TABLE tournaments
+    ALTER COLUMN start_date SET NOT NULL,
+    ALTER COLUMN end_date SET NOT NULL
+  `);
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function (knex) {
+  await knex.raw('ALTER TABLE tournaments DROP COLUMN status');
+  await knex.raw('DROP TYPE IF EXISTS tournament_status CASCADE');
+
+  await knex.raw(`
+    ALTER TABLE tournaments
+    ALTER COLUMN start_date DROP NOT NULL,
+    ALTER COLUMN end_date DROP NOT NULL
+  `);
 };
